4ab2af6427fedbda2fb4d284e11759a432c750ae
Mariana Plauska <103209930+MarianaPlauska@users.noreply.github.com>
2024-05-12 20:39:28 -0300

    Add files via upload

---
+++ carregaTabelas.sql
@@ -0,0 +1,97 @@
+CREATE TABLE Carga(
+		ID_Pedido int NOT NULL,
+		ID_Produto int NOT NULL,
+		Pedido_data varchar(10) NOT NULL,
+		Pagamento_data varchar(10) NOT NULL,
+		UPC int NOT NULL,
+		SKU int NOT NULL,
+		Nome_Produto varchar(30) NOT NULL,
+		Quant int NOT NULL,
+		Moeda varchar(15) NOT NULL,
+		Tipo_Entrega varchar(20) NOT NULL,
+		Valor float NOT NULL,
+		Email varchar(100) NOT NULL,
+		Nome_Comprador varchar(100) NOT NULL,
+		Endereco1 varchar(50) NOT NULL,
+		Endereco2 varchar(50),
+		Endereco3 varchar(50),
+		CEP int NOT NULL,
+		Cidade varchar(100) NOT NULL,
+		UF varchar(50) NOT NULL,
+		PAIS varchar(30) NOT NULL,
+		Telefone varchar(15) NOT NULL,
+		CPF VARCHAR(11) NOT NULL
+	)
+	GO
+
+	CREATE TABLE Cliente(
+		ID_cliente int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+		Nome varchar(100) NOT NULL,
+		CPF varchar(11) NOT NULL,
+		Email varchar(100) NOT NULL,
+		Telefone varchar(15) NOT NULL,
+	)
+	GO
+
+	CREATE TABLE Pedido(
+		ID_Pedido int PRIMARY KEY NOT NULL,
+		Data_Pedido date NOT NULL,
+		Pagamento_data date NOT NULL,
+		Valor_Total float NOT NULL,
+		Tipo_Entrega varchar(20) NOT NULL,
+		Moeda varchar(15) NOT NULL,
+		Endereco1 varchar(50) NOT NULL,
+		Endereco2 varchar(50),
+		Endereco3 varchar(50),
+		CEP int NOT NULL,
+		Cidade varchar(100) NOT NULL,
+		UF varchar(50) NOT NULL,
+		PAIS varchar(30) NOT NULL,
+		ID_Cliente int NOT NULL,
+		FOREIGN KEY (ID_Cliente) REFERENCES Cliente(ID_Cliente)
+	)
+	GO
+
+
+	CREATE TABLE Produto(
+		ID_Produto int PRIMARY KEY NOT NULL,
+		Valor float NOT NULL,
+		Estoque int DEFAULT 10,
+		Nome_produto varchar(30) NOT NULL,
+		UPC int NOT NULL,
+		SKU int NOT NULL,
+	)
+	GO
+
+
+	CREATE TABLE Itens_Pedidos
+	(
+		ID_Produto int NOT NULL,
+		ID_Pedido int NOT NULL,
+		Quant int NOT NULL,
+		FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto),
+		FOREIGN KEY (ID_Pedido) REFERENCES Pedido(ID_Pedido),
+	)
+	GO
+	CREATE TABLE Movimentação_Estoque
+	(
+		ID_Produto int NOT NULL,
+		ID_Pedido int NOT NULL,
+		STATUS_Pedido varchar(1) DEFAULT 'N',
+		Estoque int NOT NULL,--Estoque Destinado ao Determinado Pedido
+		FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto),
+		FOREIGN KEY (ID_Pedido) REFERENCES Pedido(ID_Pedido)
+	)
+	GO
+
+
+	CREATE TABLE Compras
+	(
+		ID_Produto int NOT NULL,
+		Nome_produto varchar(30) NOT NULL,
+		Quant int NOT NULL,
+		FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto),
+	)
+	GO
+
+	
---
+++ tabelas.sql
@@ -0,0 +1,173 @@
+IF EXISTS (SELECT 1 FROM SYS.objects WHERE TYPE = 'P' AND NAME = 'CARREGAR_CLIENTES')
+BEGIN
+    DROP PROCEDURE CARREGAR_CLIENTES
+END
+GO
+
+CREATE PROCEDURE CARREGAR_CLIENTES
+AS
+BEGIN
+    -- Carrega os clientes da tabela de carga temporária para a tabela de Clientes
+    INSERT INTO Cliente (Nome, CPF, Email, Telefone) 
+    SELECT DISTINCT 
+        C.CPF, 
+        C.Email, 
+        C.Nome_Comprador, 
+        C.Telefone 
+    FROM Carga C
+    LEFT JOIN Cliente CL ON CL.Email = C.Email 
+    WHERE CL.Email IS NULL
+END
+GO
+
+
+IF EXISTS (SELECT 1 FROM SYS.objects WHERE TYPE = 'P' AND NAME = 'CARREGAR_PRODUTOS')
+BEGIN
+    DROP PROCEDURE CARREGAR_PRODUTOS
+END
+GO
+
+CREATE PROCEDURE CARREGAR_PRODUTOS
+AS
+BEGIN
+    -- Carrega os produtos da tabela de carga temporária para a tabela de Produtos
+    INSERT INTO Produto (ID_Produto, Valor, Nome_produto, UPC, SKU) 
+    SELECT DISTINCT 
+        C.ID_Produto, 
+        C.Valor, 
+        C.Nome_Produto, 
+        C.UPC, 
+        C.SKU 
+    FROM Carga C
+    LEFT JOIN Produto P ON P.ID_Produto = C.ID_Produto
+    WHERE P.ID_Produto IS NULL
+END
+GO
+
+
+
+
+IF EXISTS (SELECT 1 FROM SYS.objects WHERE TYPE = 'P' AND NAME = 'CARREGAR_PEDIDOS')
+BEGIN
+    DROP PROCEDURE CARREGAR_PEDIDOS
+END
+GO
+
+CREATE PROCEDURE CARREGAR_PEDIDOS
+AS
+BEGIN
+    -- Carrega os pedidos da tabela de carga temporária para a tabela de Pedidos
+    INSERT INTO Pedido (ID_Pedido, Data_Pedido, Pagamento_data, Valor_Total, Tipo_Entrega, Moeda, Endereco1, Endereco2, Endereco3, CEP, Cidade, UF, PAIS, ID_Cliente) 
+    SELECT DISTINCT 
+        C.ID_Pedido, 
+        CONVERT(DATE, C.Pedido_data, 23) AS Data_Pedido, 
+        CONVERT(DATE, C.Pagamento_data, 23) AS Pagamento_data, 
+        0 AS Valor_Total, 
+        C.Tipo_Entrega, 
+        C.Moeda, 
+        C.Endereco1, 
+        C.Endereco2, 
+        C.Endereco3, 
+        C.CEP, 
+        C.Cidade, 
+        C.UF, 
+        C.PAIS, 
+        CL.ID_cliente 
+    FROM Carga C
+    LEFT JOIN Pedido P ON P.ID_Pedido = C.ID_Pedido 
+    LEFT JOIN Cliente CL ON CL.Email = C.Email
+    WHERE P.ID_Pedido IS NULL
+
+    -- Atualiza o valor total dos pedidos
+    UPDATE Pedido 
+    SET Valor_Total = (
+        SELECT SUM(C.Valor * C.Quantidade) 
+        FROM Carga C 
+        WHERE C.ID_Pedido = Pedido.ID_Pedido
+    ) 
+END
+GO
+
+
+IF EXISTS (SELECT 1 FROM SYS.objects WHERE TYPE = 'P' AND NAME = 'CARREGAR_iTENS_PEDIDOS')
+BEGIN
+    DROP PROCEDURE CARREGAR_iTENS_PEDIDOS
+END
+GO
+
+CREATE PROCEDURE CARREGAR_iTENS_PEDIDOS
+AS
+BEGIN
+    -- Carrega os itens de pedido da tabela de carga temporária para a tabela de Itens_Pedidos
+    INSERT INTO Itens_Pedidos (ID_Produto, ID_Pedido, Quantidade) 
+    SELECT DISTINCT 
+        C.ID_Produto, 
+        C.ID_Pedido, 
+        C.Quantidade
+    FROM Carga C
+    LEFT JOIN Itens_Pedidos IP ON IP.ID_Produto = C.ID_Produto 
+                                AND IP.ID_Pedido = C.ID_Pedido 
+    WHERE IP.ID_Produto IS NULL
+END
+GO
+
+
+IF EXISTS (SELECT 1 FROM SYS.objects WHERE TYPE = 'P' AND NAME = 'ADMNISTRAR_COMPRAS')
+BEGIN
+    DROP PROCEDURE ADMNISTRAR_COMPRAS
+END
+GO
+
+CREATE PROCEDURE ADMNISTRAR_COMPRAS
+AS
+BEGIN
+    -- Registra as compras de produtos com estoque negativo na tabela de Compras
+    INSERT INTO Compras(ID_Produto, Nome_produto, Quantidade) 
+    SELECT P.ID_Produto, P.Nome_produto, (P.Estoque * -1) + 10 AS Quantidade
+    FROM Produto P
+    INNER JOIN Movimentação_Estoque ME ON ME.ID_Produto = P.ID_Produto 
+    WHERE P.Estoque <= 0
+END
+GO
+
+IF EXISTS (SELECT 1 FROM SYS.objects WHERE TYPE = 'P' AND NAME = 'MOVIMENTAR_ESTOQUE')
+BEGIN
+    DROP PROCEDURE MOVIMENTAR_ESTOQUE
+END
+GO
+
+CREATE PROCEDURE MOVIMENTAR_ESTOQUE
+AS
+BEGIN
+    -- Atualiza o estoque de produtos e registra a movimentação de estoque
+    UPDATE Produto 
+    SET Estoque = Estoque - IP.Quantidade 
+    FROM Produto P 
+    INNER JOIN Itens_Pedidos IP ON P.ID_Produto = IP.ID_Produto;
+
+    INSERT INTO Movimentação_Estoque (ID_Produto, ID_Pedido, Estoque) 
+    SELECT IP.ID_Produto, IP.ID_Pedido, P.Estoque 
+    FROM Produto P 
+    INNER JOIN Itens_Pedidos IP ON IP.ID_Produto = P.ID_Produto 
+    LEFT JOIN Pedido PE ON IP.ID_Pedido = PE.ID_Pedido 
+    ORDER BY PE.Valor_Total DESC;
+
+    UPDATE Movimentação_Estoque 
+    SET Estoque = IP.Quantidade 
+    FROM Movimentação_Estoque ME 
+    INNER JOIN Itens_Pedidos IP ON ME.ID_Produto = IP.ID_Produto 
+    WHERE ME.Estoque - IP.Quantidade >= 0;
+    
+    UPDATE Movimentação_Estoque 
+    SET STATUS_Pedido = 'S', Estoque = ME.Estoque - IP.Quantidade 
+    FROM Movimentação_Estoque ME 
+    INNER JOIN Itens_Pedidos IP ON ME.ID_Pedido = IP.ID_Pedido;
+
+    -- Verifica se há pedidos não atendidos em sua totalidade e executa a procedure ADMNISTRAR_COMPRAS
+    IF(EXISTS(SELECT STATUS_Pedido FROM Movimentação_Estoque WHERE STATUS_Pedido = 'N'))
+    BEGIN
+        EXEC ADMNISTRAR_COMPRAS
+    END
+END
+GO
+
